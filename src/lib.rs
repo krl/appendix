e4e9d6b89e2fe1c8a14eef6378aabe31f512c4fd
Kristoffer Ström <kristoffer@rymdkoloni.se>
2019-01-09 09:33:07 +0100

    initial 2.0 rewrite

---
+++ src/lib.rs
@@ -0,0 +1,430 @@
+use std::cell::UnsafeCell;
+use std::fmt;
+use std::fs::OpenOptions;
+use std::hash::{Hash, Hasher};
+use std::io;
+use std::marker::PhantomData;
+use std::mem;
+use std::ops::{Deref, DerefMut};
+use std::path::{Path, PathBuf};
+
+use arrayvec::ArrayVec;
+use lazy_static::lazy_static;
+use memmap::MmapMut;
+use parking_lot::{Mutex, MutexGuard};
+use seahash::SeaHasher;
+
+const NUM_LANES: usize = 64;
+const NUM_SHARDS: usize = 1024;
+const PAGE_SIZE: usize = 4096;
+const FIRST_LANE_PAGES: usize = 64;
+
+// marker struct for shard-mutex
+struct Shard;
+
+lazy_static! {
+    static ref LOCKS: ArrayVec<[Mutex<Shard>; NUM_SHARDS]> = {
+        let mut locks = ArrayVec::new();
+        for _ in 0..NUM_SHARDS {
+            locks.push(Mutex::new(Shard))
+        }
+        locks
+    };
+}
+
+#[inline(always)]
+fn hash_val<T: Hash>(t: &T) -> u64 {
+    let mut hasher = SeaHasher::new();
+    t.hash(&mut hasher);
+    hasher.finish()
+}
+
+enum Found<'a, K, V> {
+    Some(&'a Entry<K, V>),
+    None(usize, usize, usize),
+    Invalid(usize, usize, usize),
+}
+
+pub type AlreadyThere = bool;
+
+#[derive(Debug)]
+pub struct Index<K, V> {
+    lanes: UnsafeCell<ArrayVec<[MmapMut; NUM_LANES]>>,
+    path: PathBuf,
+    pages: Mutex<u64>,
+    _marker: PhantomData<(K, V)>,
+}
+
+unsafe impl<K, V> Send for Index<K, V> {}
+unsafe impl<K, V> Sync for Index<K, V> {}
+
+#[derive(Debug)]
+struct Entry<K, V> {
+    key: K,
+    val: V,
+    next: u64,
+    kv_checksum: u64,
+    next_checksum: u64,
+}
+
+struct EntryMut<'a, K, V> {
+    entry: &'a mut Entry<K, V>,
+    _lock: MutexGuard<'a, Shard>,
+}
+
+impl<'a, K, V> Deref for EntryMut<'a, K, V> {
+    type Target = Entry<K, V>;
+    fn deref(&self) -> &Self::Target {
+        &self.entry
+    }
+}
+
+impl<'a, K, V> DerefMut for EntryMut<'a, K, V> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.entry
+    }
+}
+
+impl<K: Hash, V: Hash> Entry<K, V> {
+    fn new(key: K, val: V) -> Self {
+        let kv_checksum = hash_val(&key).wrapping_add(hash_val(&val));
+        let entry = Entry {
+            key,
+            val,
+            kv_checksum,
+            next: 0,
+            next_checksum: 0 + 1,
+        };
+        debug_assert!(entry.valid());
+        entry
+    }
+
+    fn valid(&self) -> bool {
+        if hash_val(&self.key).wrapping_add(hash_val(&self.val))
+            == self.kv_checksum
+            && self.next + 1 == self.next_checksum
+        {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn set_next<I: Into<u64>>(&mut self, next: I) {
+        let next = next.into();
+        self.next = next;
+        self.next_checksum = next + 1;
+    }
+}
+
+impl<K: Hash + Copy + PartialEq + fmt::Debug, V: Hash + Copy + fmt::Debug>
+    Index<K, V>
+{
+    pub fn new<P: AsRef<Path>>(path: &P) -> io::Result<Self> {
+        let lanes = ArrayVec::new();
+        // for i in 0..NUM_LANES {
+        //     let path: &Path = path.as_ref();
+        //     let mut pathbuf = PathBuf::from(path);
+        //     pathbuf.push(&format!("{}", i));
+        //     println!("creating {:?}", pathbuf);
+        //     let file = OpenOptions::new()
+        //         .read(true)
+        //         .write(true)
+        //         .create(true)
+        //         .open(&pathbuf)?;
+        //     file.set_len(1);
+        //     unsafe { maps.push(MmapMut::map_mut(&file)?) }
+        // }
+
+        let index = Index {
+            lanes: UnsafeCell::new(lanes),
+            path: PathBuf::from(path.as_ref()),
+            pages: Mutex::new(0),
+            _marker: PhantomData,
+        };
+        // FIXME: support loading existing indicies
+        assert_eq!(index.new_page()?, 0);
+        Ok(index)
+    }
+
+    #[inline(always)]
+    fn entries_per_page() -> usize {
+        PAGE_SIZE / mem::size_of::<Entry<K, V>>()
+    }
+
+    // calculates the slot in the page this hashed key would
+    // occupy at a certain depth
+    #[inline(always)]
+    fn slot(key_hash: u64, depth: usize) -> usize {
+        (hash_val(&(key_hash + depth as u64)) % Self::entries_per_page() as u64)
+            as usize
+    }
+
+    // produces following output over page with FIRST_LANE_PAGES = 2
+    // (0, 0), (0, 1),
+    // (1, 0), (1, 1), (1, 2), (1, 3),
+    // (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7),
+    // ... and so on and so forth ...
+    #[inline(always)]
+    fn lane_page(page: usize) -> (usize, usize) {
+        let usize_bits = mem::size_of::<usize>() * 8;
+        let i = page / FIRST_LANE_PAGES + 1;
+        let row = usize_bits - i.leading_zeros() as usize - 1;
+        (row, page - (2usize.pow(row as u32) - 1) * FIRST_LANE_PAGES)
+    }
+
+    fn new_lane(&self) -> io::Result<()> {
+        // FIXME, make exponential
+        let lanes_ptr = self.lanes.get();
+        let lane_nr = unsafe { (*lanes_ptr).len() };
+
+        let num_pages = 2_usize.pow(lane_nr as u32 + 1) * FIRST_LANE_PAGES;
+
+        let mut path = self.path.clone();
+        path.push(format!("{:02x}", lane_nr));
+
+        let file_len = PAGE_SIZE as u64 * num_pages as u64;
+
+        // println!("creating file {:?} for {} pages", path, num_pages);
+        // println!("lane {} size {}", lane_nr, file_len);
+
+        let file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(&path)?;
+
+        file.set_len(file_len)?;
+
+        unsafe { (*lanes_ptr).push(MmapMut::map_mut(&file)?) }
+        Ok(())
+    }
+
+    fn new_page(&self) -> io::Result<u64> {
+        let mut page_nr = self.pages.lock();
+
+        let (_, offset) = Self::lane_page(*page_nr as usize);
+
+        if offset == 0 {
+            // create new lane
+            self.new_lane()?
+        }
+
+        let new_page_nr = *page_nr;
+        *page_nr += 1;
+
+        Ok(new_page_nr)
+    }
+
+    fn entry(&self, lane: usize, page: usize, slot: usize) -> &Entry<K, V> {
+        // Get a reference to the `Entry`
+        let page_ofs = PAGE_SIZE * page;
+        let slot_ofs = page_ofs + slot * mem::size_of::<Entry<K, V>>();
+
+        // if slot_ofs > 30000 {
+        //     println!("lane page slot {} {} {}", lane, page, slot);
+        //     println!("get entry with slot_ofs: {}", slot_ofs);
+        // }
+        unsafe {
+            mem::transmute(
+                (*self.lanes.get())[lane].as_ptr().offset(slot_ofs as isize),
+            )
+        }
+    }
+
+    // Get a mutable reference to the `Entry`,
+    // locking the corresponding shard.
+    fn entry_mut(
+        &self,
+        lane: usize,
+        page: usize,
+        slot: usize,
+    ) -> EntryMut<K, V> {
+        let shard = (page ^ slot) % NUM_SHARDS;
+        // Lock the entry for writing
+        let lock = LOCKS[shard].lock();
+
+        let page_ofs = PAGE_SIZE * page;
+        let slot_ofs = page_ofs + slot * mem::size_of::<Entry<K, V>>();
+        EntryMut {
+            entry: unsafe {
+                mem::transmute(
+                    (*self.lanes.get())[lane]
+                        .as_ptr()
+                        .offset(slot_ofs as isize),
+                )
+            },
+            _lock: lock,
+        }
+    }
+
+    fn find_key(&self, k: &K) -> io::Result<Found<K, V>> {
+        let mut depth = 0;
+        let mut abs_page = 0;
+        loop {
+            let hash = hash_val(&k);
+            let slot = Self::slot(hash, depth);
+
+            let (lane, page) = Self::lane_page(abs_page);
+            let entry = self.entry(lane, page, slot);
+
+            if !entry.valid() {
+                return Ok(Found::Invalid(lane, page, slot));
+            }
+
+            if &entry.key == k {
+                return Ok(Found::Some(entry));
+            } else if entry.next == 0 {
+                return Ok(Found::None(lane, page, slot));
+            } else {
+                abs_page = entry.next as usize;
+            }
+
+            depth += 1;
+        }
+    }
+
+    pub fn insert(&self, key: K, val: V) -> io::Result<()> {
+        match self.find_key(&key)? {
+            Found::Some(_) => {
+                // no-op
+                Ok(())
+            }
+            Found::Invalid(lane, page, slot) => {
+                let mut entry = self.entry_mut(lane, page, slot);
+
+                if entry.valid() && entry.next != 0 {
+                    // Someone already wrote here, recurse!
+                    // We accept the performance hit of re-traversing
+                    // the whole tree, since this case is uncommon,
+                    // and makes the implementation simpler.
+                    mem::drop(entry);
+                    self.insert(key, val)
+                } else {
+                    *entry = Entry::new(key, val);
+                    return Ok(());
+                }
+            }
+            Found::None(lane, page, slot) => {
+                let mut entry = self.entry_mut(lane, page, slot);
+                if entry.next != 0 {
+                    // again, another thread was here before us
+                } else {
+                    entry.set_next(self.new_page()?);
+                }
+                // recurse
+                mem::drop(entry);
+                self.insert(key, val)
+            }
+        }
+    }
+
+    pub fn get(&self, key: &K) -> io::Result<Option<&V>> {
+        match self.find_key(key)? {
+            Found::Some(entry) => Ok(Some(&entry.val)),
+            _ => Ok(None),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+    use std::thread;
+
+    use rand::{seq::SliceRandom, thread_rng};
+    use tempfile::tempdir;
+
+    use super::*;
+
+    #[test]
+    fn simple() {
+        let dir = tempdir().unwrap();
+        let index = Index::new(&dir).unwrap();
+        index.insert(0, 0).unwrap();
+        assert_eq!(index.get(&0).unwrap(), Some(&0));
+    }
+
+    const N: u64 = 1024 * 256;
+
+    #[test]
+    fn multiple() {
+        let dir = tempdir().unwrap();
+        let index = Index::new(&dir).unwrap();
+        for i in 0..N {
+            index.insert(i, i).unwrap();
+        }
+        for i in 0..N {
+            assert_eq!(index.get(&i).unwrap(), Some(&i));
+        }
+    }
+
+    const N_THREADS: usize = 8;
+
+    #[test]
+    fn stress() {
+        let dir = tempdir().unwrap();
+        let index = Arc::new(Index::new(&dir).unwrap());
+
+        let mut all_indicies = vec![];
+        for i in 0..N {
+            all_indicies.push(i);
+        }
+
+        let mut rng = thread_rng();
+
+        // shuffle the order of the writes
+        let mut shuffles_write = vec![];
+        for _ in 0..N_THREADS {
+            let mut new = all_indicies.clone();
+            SliceRandom::shuffle(&mut new[..], &mut rng);
+            shuffles_write.push(new);
+        }
+
+        // shuffle the order of the reads
+        let mut shuffles_read = vec![];
+        for _ in 0..N_THREADS {
+            let mut new = all_indicies.clone();
+            SliceRandom::shuffle(&mut new[..], &mut rng);
+            shuffles_read.push(new);
+        }
+
+        let mut threads_running = vec![];
+
+        for i in 0..N_THREADS {
+            // shuffled write
+            let shuffle_write = mem::replace(&mut shuffles_write[i], vec![]);
+            let index_write = index.clone();
+
+            // write threads
+            threads_running.push(thread::spawn(move || {
+                for write in shuffle_write {
+                    index_write.insert(write, write).unwrap()
+                }
+            }));
+
+            // shuffled reads
+            let shuffle_read = mem::replace(&mut shuffles_read[i], vec![]);
+            let index_read = index.clone();
+
+            // read threads
+            threads_running.push(thread::spawn(move || {
+                for read in shuffle_read {
+                    match index_read.get(&read).unwrap() {
+                        Some(val) => assert_eq!(val, &read),
+                        None => (),
+                    }
+                }
+            }));
+        }
+
+        // make sure all threads finish successfully
+        for thread in threads_running {
+            thread.join().unwrap()
+        }
+
+        for i in 0..N {
+            assert_eq!(index.get(&i).unwrap(), Some(&i));
+        }
+    }
+}
